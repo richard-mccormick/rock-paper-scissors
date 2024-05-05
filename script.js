c1b113e8537be4ef29b76b7b84c0c438425ed5ec
Richard McCormick <mccormickri38@gmail.com>
2024-05-05 01:06:57 -0400

    Add function for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("ITS WORKING!!!");
+
+// Code for computer choice
+
+function getComputerChoice(){
+    let compChoice = Math.random();
+
+    if (compChoice < 1/3) {
+        return 'rock';
+    } else if (compChoice > 2/3 ) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
